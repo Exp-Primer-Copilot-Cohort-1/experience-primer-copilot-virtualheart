7a70198e946240d57fc9f433985869cf5d06d4b8
Dhanajayan K <72462261+virtualheart@users.noreply.github.com>
2024-03-09 04:09:45 +0000

    Copilot second commit

--- comments.js
+++ comments.js
@@ -1,20 +1,24 @@
-// Create web server with express
-// Create a route to handle GET requests to /comments
-// Return a JSON object with a key of comments and a value of an array of comments
-// Each comment should be an object with a key of body and a value of the comment itself
+// Create web server that has a route for /comments. This route should accept POST requests and store comments in an array. 
+// It should return a JSON object with all the comments when it receives a GET request to /comments.
 
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+
+let comments = [];
+
+app.post('/comments', (req, res) => {
+    let comment = req.body.comment;
+    comments.push(comment);
+    res.send("Comment added");
+});
 
 app.get('/comments', (req, res) => {
-    res.json({
-        comments: [
-            {body: "This is awesome!"},
-            {body: "I like this!"}
-        ]
-    });
+    res.json(comments);
 });
 
 app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+    console.log('Server is listening on port 3000');
 });
